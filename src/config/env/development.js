bd83b3a9f95ce6d2af2f34223d7766147ac90c4a
Setyo Nugroho <setyo.nugroho1337@gmail.com>
2017-08-18 17:30:44 +0700

    Add redis configuration

--- src/config/env/development.js
+++ src/config/env/development.js
@@ -2,5 +2,6 @@ module.exports = {
   env: 'development',
   db: 'mongodb://localhost/bitrix',
   port: 3000,
-  redisPort: 6379
+  redis_host: '127.0.01',
+  redis_port: 6379
 };
--- src/config/redis.js
+++ src/config/redis.js
@@ -1,6 +1,11 @@
 const bluebird = require('bluebird');
 const redis = require("redis");
-const client = redis.createClient();
+const config = require('./env')
+const client = redis.createClient({
+    host: config.redis_host,
+    port: config.redis_port
+});
+
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
