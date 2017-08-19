b316fb90d2c345af5cd31942175205ce197ba039
Setyo Nugroho <setyo.nugroho1337@gmail.com>
2017-08-19 20:43:56 +0700

    Using url for redis configuration

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,8 +1,7 @@
 const bluebird = require('bluebird');
 const redis = require("redis");
 const client = redis.createClient({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT
+    url: process.env.REDIS_URL
 });
 
 
