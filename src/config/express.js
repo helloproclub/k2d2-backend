fef99e38b64347c74341adace727dddb63a26fd1
Setyo Nugroho <setyo.nugroho1337@gmail.com>
2017-08-20 15:53:11 +0700

    Setup CORS

--- src/config/express.js
+++ src/config/express.js
@@ -9,6 +9,16 @@ const app = express()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 
+// CORS
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");  
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");  
+    // res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+  
+
 //API Version 1
 app.use('/api', v1)
 
